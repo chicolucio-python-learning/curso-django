d66d0d962d084735cefeb72c6726afae75b07164
Francisco Bustamante <23560423+chicolucio@users.noreply.github.com>
2020-05-22 10:58:22 -0300

    template tag static

--- pypro/settings.py
+++ pypro/settings.py
@@ -35,7 +35,7 @@ ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
 AUTH_USER_MODEL = 'base.User'
 
 # STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-# COLLECTFAST_STRATEGY = 'collectfast.strategies.boto3.Boto3Strategy'
+COLLECTFAST_STRATEGY = 'collectfast.strategies.boto3.Boto3Strategy'
 
 # Application definition
 
