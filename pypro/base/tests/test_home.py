9e8a6edcfd66b4e0b7b0bfdcae921a905d164efc
Francisco Bustamante <23560423+chicolucio@users.noreply.github.com>
2020-06-08 14:52:17 -0300

    Composição de templates

--- pypro/base/tests/test_home.py
+++ pypro/base/tests/test_home.py
@@ -22,3 +22,7 @@ def test_title(resp):
 
 def test_home_link(resp):
     assert_contains(resp, f'href="{reverse("base:home")}">Python Pro</a>')
+
+    
+def test_email_link(resp):
+    assert_contains(resp, 'href="mailto:chicolucio@whatever.com"')
